79d39d265836f958eb1a8b532e7f81da174128a4
Virginie Guemas <virginie.guemas@meteo.fr>
2026-08-28 15:10:45 +0200

    Final tweak before testing

--- comp_seaiceindex.py
+++ comp_seaiceindex.py
@@ -1,5 +1,5 @@
-# This script averages a variable on each of the seas described into a mask    #
-# file.                                                                        #
+# This script computes the area-average of a variable in each of the seas      #
+# described into a mask file. The input variable can be 2d/3d/4d.              #
 #                                                                              #
 # History : 2026 - initial version by Virginie Guemas                          #
 ################################################################################
@@ -11,34 +11,57 @@ import getpass
 import argparse
 
 # Input arguments
-parser = argparse.ArgumentParser(description='Average a variable on each of the seas described into a mask file.')
-parser.add_argument('--maskfile', type=str, default="/home/guemas/tmp/test_regions/mask.ArcticSeas.cnrmcm7.nc", help='Path to the mask netcdf file')
-parser.add_argument('--datafile', type=str, required=True, help='Path to the input data netcdf file')
-parser.add_argument('--variable', type=str, required=True, help='Name of the variable to average')
-parser.add_argument('--outfile', type=str, default="sia_per_sea.nc", help='Path to the output netcdf file')
+parser = argparse.ArgumentParser(description='Area-Averages a variable (2d/3d/4d) in each of the seas described into the mask file')
+parser.add_argument('--data', type=str, required=True, help='Path to the input data netcdf file')
+parser.add_argument('--var', type=str, required=True, help='Name of the variable to average')
+parser.add_argument('--mask', type=str, default="/home/guemas/tmp/test_regions/mask.ArcticSeas.cnrmcm7.nc", help='Path to the mask netcdf file (containing various sea masks (1/0)')
+parser.add_argument('--grid', type=str, default="/home/guemas/mytools/cnrmcm7/masks/mesh_mask.nc", help='Path to the grid description netcdf file')
+parser.add_argument('--dxvar', type=str, default="e1t", help='Name of the variable containing the length of grid cells along x dimension (in m)')
+parser.add_argument('--dyvar', type=str, default="e2t", help='Name of the variable containing the length of grid cells along y dimension (in m)')
+parser.add_argument('--out', type=str, default="sia_per_sea.nc", help='Path to the output netcdf file')
+parser.add_argument('--NA', type=str, default=False, help='Skip NA (True/False)')
 args = parser.parse_args()
 
-maskfile = args.maskfile
-datafile = args.datafile
-variable = args.variable
-outfile = args.outfile
+datafile = args.data
+variable = args.var
+maskfile = args.mask
+gridfile = args.grid
+dxvar    = args.dxvar
+dyvar    = args.dyvar
+outfile  = args.out
+skip_na  = args.NA
 
 # Open input files
-if os.path.exists(maskfile):
-  maskdataset = xr.open_dataset(maskfile)
-else:
-  sys.exit('Mask file is missing')
-
 if os.path.exists(datafile):
   data = xr.open_dataset(datafile)
 else:
   sys.exit('Data file is missing')
 
-# Read input variable
+if os.path.exists(maskfile):
+  maskdataset = xr.open_dataset(maskfile)
+else:
+  sys.exit('Mask file is missing')
+
+if os.path.exists(gridfile):
+  grid = xr.open_dataset(gridfile)
+else:
+  sys.exit('Grid file is missing')
+
+# Read input variables
 if variable in data:
   field = data[variable]
 else:
   sys.exit('Variable missing from input file')
+
+if dxvar in grid:
+  dx = grid[dxvar]
+else:
+  sys.exit('dxvar missing from input file')
+
+if dyvar in grid:
+  dy = grid[dyvar]
+else:
+  sys.exit('dyvar missing from input file')
 
 # List of sea masks
 lstmasks = maskdataset.data_vars
@@ -54,6 +77,12 @@ for sea in lstmasks:
   elif mask.dims != ref_dims:
     sys.exit("Mask " + sea + " does not share the same dimensions as the other masks")
 
+# Check that the grid descrption file is bi-dimensional with the same x and y dimensions
+if dx.ndim != 2 or dy.ndim != 2:
+  sys.exit("dxvar or dyvar is not bi-dimensional")
+elif dx.dims != ref_dims or dy.dims!= ref_dims:
+  sys.exit("dxvar or dyvar does not share the same dimensions as the masks")
+
 # Find the dimensions common between field and the masks
 dims_to_reduce = [dim for dim in ref_dims if dim in field.dims]
 if len(dims_to_reduce) != 2:
@@ -62,15 +91,14 @@ if len(dims_to_reduce) != 2:
 # Define output dataset containing average over each sea
 outdataset = xr.Dataset(attrs=dict(description = 'Average of variable ' + variable + ' for individual seas and regions', based_on_mask = maskfile, using_variable_file = datafile, creation_date = str(datetime.datetime.now()), created_by = getpass.getuser()))
 
-# Compute the average over each sea
+# Compute the area-averaged variable over each sea
+area=dx*dy
 for sea in lstmasks:
   mask = maskdataset[sea]
   # Apply mask
   masked_field = field.where(mask == 1)
   # Compute the average over the dimensions common between field and mask
-  mean_on_sea = masked_field.mean(dim=dims_to_reduce, skipna=True)
-  # Store in the output dataset
-  outdataset[sea] = xr.DataArray(mean_on_sea.values, attrs = dict(sea_name = mask.long_name))
+  outdataset[sea]= masked_field.weighted(area).mean(dim=dims_to_reduce, skipna=skip_na).assign_attrs(dict(sea_name = mask.long_name))
 
 # Write the output netcdf file
 outdataset.to_netcdf(outfile)
